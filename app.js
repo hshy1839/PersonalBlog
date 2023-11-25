93bbba9fbf05a47429e0ad908b76eaa8f5872ff7
hshy1839 <hongjeongmin1839@gmail.com>
2023-11-26 04:52:25 +0900

    index 위치 옮김

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var server = http.createServer(app).listen(port, function () {
   console.log(`listening on http://localhost:${port}`);
 });
 
-app.use('/', express.static("./public"));
+app.use('/', express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname+"/public/index.html");
+  res.sendFile(__dirname+"/index.html");
 });
 
 app.get('/main', function(req, res) {
-  res.sendFile(__dirname+"/public/index.html");
+  res.sendFile(__dirname+"/index.html");
 });
 
