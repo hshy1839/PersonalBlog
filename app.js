90cc9e4b3721cfbb1cf14a074597f4bcf4f5efef
hshy1839 <hongjeongmin1839@gmail.com>
2023-11-29 14:22:46 +0900

    .

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-const sql = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
 
 const port = 7777;
-
-const db = new sql.Database('users.db');
-
-db.serialize(() => {
-  db.run(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY, 
-    username TEXT,
-    password TEXT,
-    name TEXT,
-    email TEXT
-  )`);
-});
-
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
 
 app.get('/', function(req, res) { res.sendFile(__dirname+"/views/login.html");});
 app.use('/', express.static(__dirname + "/public"));
@@ -32,3 +14,4 @@ app.get('/login/signup', function(req, res) { res.sendFile(__dirname+"/views/sig
 var server = http.createServer(app).listen(port, function () {
   console.log(`listening on http://localhost:${port}`);
 });
+
