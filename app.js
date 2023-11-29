91e4f40b56879bd16fb45108b4b0e452418880ad
hongjeongmin <hongjeongmin1839@gmail.com>
2023-11-29 17:25:08 +0900

    app.js실행 오류 수정

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-var express = require('express');
+const express = require('express');
 const http = require('http');
 const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
-const { squelize } = require('./models');
+const { sequelize } = require('./models'); // squelize를 sequelize로 수정
 
-var app = express();
+const app = express();
 
-var port = 7777;
+const port = 7777;
 app.set('port', process.env.PORT || 3001);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
-  express : app,
-  watch : true,
+  express: app,
+  watch: true,
 });
-squelize.sync({ force:false}).then(() => { console.log('DB 연결성공');}).catch((err) => { console.log(err);});
+sequelize.sync({ force: false }).then(() => {
+  console.log('DB 연결 성공');
+}).catch((err) => {
+  console.error(err); // 에러를 콘솔에 출력
+});
+
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
+app.use((err, req, res, next) => { // 에러 핸들링 미들웨어에서 err을 정의
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
   res.render('error');
-})
+});
 
-var express = require('express');
-var app = express();
-var http = require('http');
-
-const port = 7777;
-
-app.get('/', function(req, res) { res.sendFile(__dirname+"/views/login.html");});
+app.get('/', function (req, res) { res.sendFile(__dirname + "/views/login.html"); });
 app.use('/', express.static(__dirname + "/public"));
 app.use('/', express.static(__dirname + "/views"));
-app.get('/login', function(req, res) { res.sendFile(__dirname+"/views/login.html");});
-app.get('/main', function(req, res) { res.sendFile(__dirname+"/views/index.html");});
-app.get('/login/signup', function(req, res) { res.sendFile(__dirname+"/views/signup.html");});
+app.get('/login', function (req, res) { res.sendFile(__dirname + "/views/login.html"); });
+app.get('/main', function (req, res) { res.sendFile(__dirname + "/views/index.html"); });
+app.get('/login/signup', function (req, res) { res.sendFile(__dirname + "/views/signup.html"); });
 
 var server = http.createServer(app).listen(port, function () {
   console.log(`listening on http://localhost:${port}`);
 });
-
